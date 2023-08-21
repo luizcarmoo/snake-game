15e8162afc065d988ab506db27a07dcf08aecfe1
Luiz Carmo <luiz.pcarmo@gmail.com>
2023-08-20 21:41:39 -0300

    feat: inserted some styles and scripts

---
+++ js/script.js
@@ -0,0 +1,79 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+const size = 30
+
+const snake = [{ x: 200, y: 200 },
+    { x:230, y:200 }]
+
+let direction, loopId
+
+const drawSnake = () => {
+    ctx.fillStyle = "#00B01D"
+    
+    snake.forEach((position, index) => {
+        
+        if (index == snake.length - 1) {
+            ctx.fillStyle = "#00FF40"
+        }
+
+        ctx.fillRect(position.x, position.y, size, size)
+    })
+}
+
+const moveSnake = () => {
+    if (!direction) return
+
+    const head = snake[snake.length -1]
+
+    if (direction == "right") {
+        snake.push({ x: head.x + size, y: head.y })
+    }
+  
+    if (direction == "left") {
+        snake.push({ x: head.x - size, y: head.y })
+    }
+    
+    if (direction == "down") {
+        snake.push({ x: head.x, y: head.y + size })
+    }
+    
+    if (direction == "up") {
+        snake.push({ x: head.x, y: head.y - size })
+    }
+
+    snake.shift()
+}
+
+const gameLoop = () => {
+    clearInterval(loopId)
+    
+    ctx.clearRect(0, 0, 600, 600)
+    moveSnake()
+    drawSnake()
+    
+    loopId = setTimeout(() => {
+        gameLoop()
+    }, 300)
+}
+
+gameLoop()
+
+document.addEventListener("keydown", ({ key }) => {
+     if (key == "ArrowRight" && direction != "left") {
+         direction = "right"
+     }
+
+     if (key == "ArrowLeft" && direction != "right") {
+         direction = "left"
+     }
+
+     if (key == "ArrowDown" && direction != "up") {
+         direction = "down"
+     }
+
+     if (key == "ArrowUp" && direction != "down") {
+         direction = "up"
+     }
+
+})
