a58493583e0315c9dff72970a294a87cf87a3047
Luiz Carmo <luiz.pcarmo@gmail.com>
2023-08-20 23:54:44 -0300

    feat: inserted on script grids, food and snake grow

--- js/script.js
+++ js/script.js
@@ -3,8 +3,41 @@ const ctx = canvas.getContext("2d")
 
 const size = 30
 
-const snake = [{ x: 200, y: 200 },
-    { x:230, y:200 }]
+const snake = [{ x: 270, y: 270 }]
+
+const randomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30 
+}
+
+const randomColor = () => {
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+const food = {
+    x: randomPosition(), 
+    y: randomPosition(),
+    color: randomColor()
+}
+
+const drawFood = () => {
+
+    const { x, y, color} = food
+
+    ctx.shadowColor = color
+    ctx.shadowBlur = 7
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, size, size)
+    ctx.shadowBlur = 0
+}
 
 let direction, loopId
 
@@ -45,12 +78,54 @@ const moveSnake = () => {
     snake.shift()
 }
 
+const drawGrid = () => {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#191919"
+    
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+        
+        ctx.beginPath()
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+        ctx.stroke()
+    }
+
+
+}
+
+const checkEat = () => {
+    const head = snake[snake.length -1]
+
+    if (head.x == food.x && head.y == food.y){
+        snake.push(head)
+
+        let x = randomPosition()
+        let y = randomPosition()
+
+        while (snake.find((position) => position.x == x && position.y == y )) {
+            x = randomPosition()
+            y = randomPosition()
+        }
+
+        food.x = x
+        food.y = y
+        food.color = randomColor()
+    }
+}
+
 const gameLoop = () => {
     clearInterval(loopId)
     
     ctx.clearRect(0, 0, 600, 600)
+    drawGrid()
+    drawFood()
     moveSnake()
     drawSnake()
+    checkEat()
     
     loopId = setTimeout(() => {
         gameLoop()
